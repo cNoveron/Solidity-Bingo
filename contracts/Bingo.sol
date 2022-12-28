b508a6826b757c5e894392f0221209b790613819
cNoveron <carlos.escom.ipn@gmail.com>
2022-12-27 22:58:04 -0600

    contracts/Bingo: create contract skelleton

---
+++ contracts/Bingo.sol
@@ -0,0 +1,54 @@
+//SPDX-License-Identifier: UNLICENSED
+
+// Solidity files have to start with this pragma.
+// It will be used by the Solidity compiler to validate its version.
+pragma solidity ^0.8.9;
+
+// We import this library to be able to use console.log
+import "hardhat/console.sol";
+
+
+// This is the main building block for smart contracts.
+contract Bingo {
+    function newGame() 
+        external
+        returns(uint256 gameId)
+    {
+
+    }
+
+
+    function start(uint256 gameId)
+        external
+    {
+        
+    }
+
+
+    function draw(uint256 gameId)
+        external
+    {
+        
+    }
+
+
+    function join(uint256 gameId)  
+        external
+    {
+        
+    }
+
+
+    function mark(uint256 gameId)
+        external
+    {
+        
+    }
+
+
+    function claim()
+        external 
+    {
+        
+    }
+}
